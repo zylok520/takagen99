7c547bc4cd889ebfbff635a99fa90ae1e4857934
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2024-08-24 00:42:02 +0800

    增加在线更新功能

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -1,8 +1,13 @@
 package com.github.tvbox.osc.ui.dialog;
 
 import android.content.Context;
+import android.widget.TextView;
+import android.os.Bundle;
+import com.github.tvbox.osc.BuildConfig;
 
 import androidx.annotation.NonNull;
+import com.github.tvbox.osc.ui.xupdate.Constants;
+import com.xuexiang.xupdate.easy.EasyUpdate;
 
 import com.github.tvbox.osc.R;
 
@@ -13,5 +18,21 @@ public class AboutDialog extends BaseDialog {
     public AboutDialog(@NonNull @NotNull Context context) {
         super(context);
         setContentView(R.layout.dialog_about);
+        findViewById(R.id.xupdate).setOnClickListener(view -> {
+            switch (view.getId()) {
+                case R.id.xupdate:
+                    EasyUpdate.checkUpdate(getContext(), Constants.UPDATE_DEFAULT_URL);
+                    break;
+                                }
+        });
+    }
+    private TextView appVersion;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        appVersion = (TextView)findViewById(R.id.app_version);
+        appVersion.setText(BuildConfig.VERSION_NAME);
+
     }
 }
