4b6515b08eae0dc8c918854a9bd34df4547b82c9
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2024-08-23 23:36:34 +0800

    增加DOH节点

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -112,6 +112,9 @@ public class OkGoHelper {
             case 6: {
                 return "https://dns.quad9.net/dns-query";
             }
+			case 7: {
+                return "https://1.1.1.1/dns-query";
+            }
         }
         return "";
     }
@@ -124,6 +127,7 @@ public class OkGoHelper {
         dnsHttpsList.add("Google");
         dnsHttpsList.add("AdGuard");
         dnsHttpsList.add("Quad9");
+        dnsHttpsList.add("CloudFlare");
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkExoPlayer");
         if (Hawk.get(HawkConfig.DEBUG_OPEN, false)) {
